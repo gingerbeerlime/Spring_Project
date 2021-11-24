bd163f1d5850f3f6c9a2e2d1e51dc338fa8ee5a6
sumin <lsumin1127@gmail.com>
2021-11-24 09:35:12 +0900

    First Commit

---
+++ src/main/java/com/bbs/dao/UsersDAOImpl.java
@@ -0,0 +1,15 @@
+package com.bbs.dao;
+
+import org.springframework.stereotype.Repository;
+
+//저장소 역할
+@Repository
+public class UsersDAOImpl implements UsersDAO {
+
+	@Override
+	public int check_id(String user_id) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
